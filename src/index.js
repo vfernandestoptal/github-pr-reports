15f5185abb1f64b2ddcbe0edcdc68d1bd437086c
Vitor Fernandes <vitor.fernandes@toptal.com>
2017-08-02 22:43:23 +0100

    feat(reports) - add user time to merge report

--- src/index.js
+++ src/index.js
@@ -7,21 +7,27 @@ const moment = require('moment');
 const ghTokens = require('./githubTokenService');
 const pullRequests = require('./githubPullRequests');
 
-ghTokens.getToken((err, token) => {
-    if (err) {
-        return console.log('ERROR', err);
-    }
+// ghTokens.getToken((err, token) => {
+//     if (err) {
+//         return console.log('ERROR', err);
+//     }
 
-    pullRequests.getPullRequestsData({
-        token,
-        organization: config.get('project.organization'),
-        repository: config.get('project.repo'),
-        startDate: moment.utc('2017-07-28').startOf('day'),
-        endDate: moment.utc('2017-07-28').endOf('day'),
-    })
-        .then(data => {
-            console.log('DATA', JSON.stringify(data, null, 2));
-        })
-        .catch(error => console.log('ERROR', error));
+//     pullRequests.getPullRequestsData({
+//         token,
+//         organization: config.get('project.organization'),
+//         repository: config.get('project.repo'),
+//         startDate: moment.utc('2017-07-28').startOf('day'),
+//         endDate: moment.utc('2017-07-28').endOf('day'),
+//     })
+//         .then(data => {
+//             console.log('DATA', JSON.stringify(data, null, 2));
+//         })
+//         .catch(error => console.log('ERROR', error));
 
-});
+// });
+
+const userTimeToMergeReport = require('./reports/userTimeToMerge');
+const path = require('path');
+const data = JSON.parse(require('fs').readFileSync(path.join(__dirname, '..', 'datasamples', 'pullRequestsData.json'), 'utf-8'));
+
+console.log(userTimeToMergeReport.generate(data));
